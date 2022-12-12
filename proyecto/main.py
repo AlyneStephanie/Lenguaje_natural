eeeed44709b739f3e48b3005ca459e810fe0f487
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2022-12-12 13:57:36 -0600

    iniciando con el proyecto - estructura y primeros pasos

---
+++ proyecto/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+def main():
+
+    #cargamos el dataset
+    dataFrame = pd.read_excel("./Rest_Mex_2022_Sentiment_Analysis_Track_Train.xlsx") #<- Requiere que se instale openpyxl -> pip install openpyxl
+
+    print(dataFrame)
+
+    withoutTitles = dataFrame.drop('Title', axis=1)
+    without_Opinions_And_Opinions = withoutTitles.drop('Opinion', axis=1)  #<- dataset sin titulos ni opiniones
+
+    titles = dataFrame['Title'].values
+    opinions = dataFrame['Opinion'].values
+
+
+    print("dataframe sin titulos ni opiniones:\n\n")
+    print(without_Opinions_And_Opinions)
+
+    #aplicamos tokenizacion y lematización al texto de las columnas  Title y Opinion
+
+    
+
+    return 0
+
+if __name__ == "__main__":
+    main()
