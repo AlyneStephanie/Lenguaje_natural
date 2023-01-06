3e4a1c07132eac8e09cad8b3055d03bae68fb05c
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2023-01-06 13:06:42 -0600

    nueva logica con clasificadores bayesianos fase beta

--- proyecto/librerias/dataSets.py
+++ proyecto/librerias/dataSets.py
@@ -58,7 +58,7 @@ def crearConjuntosDeValidacion(pliegues, X, y):
 
 	#Separa corpus en conjunto de entrenamiento y prueba
 
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle = False)	
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle = True)	
 	
 	'''print('\n Dataset\n', df)
 	print('\n Corpus')
