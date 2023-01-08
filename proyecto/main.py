51960fb077e496d7f8aaee6acb40c29fda680149
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2023-01-07 23:48:35 -0600

    fase beta 3

--- proyecto/main.py
+++ proyecto/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from librerias import tokenizadorLematizador
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import classification_report
@@ -96,9 +96,15 @@ def main():
 
     representacionVectorial = vectorizacion.VectorizarFrec(x)
 
-    dataset = dataSets.crearConjuntosDeValidacion(5, representacionVectorial, target)
+    #dataset = dataSets.crearConjuntosDeValidacion(1, representacionVectorial, target)
 
-    print("se ha vectorizado")
+    #probando con un conjunto de entrenamiento y uno de prueba------------------------------------------------
+
+    X_train, X_test, y_train, y_test = train_test_split(representacionVectorial, target,test_size=0.2, shuffle=True)
+
+    #---------------------------------------------------------------------------------------------------------
+
+    #print("se ha vectorizado")
 
 
     # #reporte de la clasificacion
@@ -116,86 +122,36 @@ def main():
     # plt.show()
 
     i = 0
-    clf = GaussianNB()
+    clf = MultinomialNB()
+    # clf.fit(X_train.toarray(), y_train)
+    contador1 = 0
+    contador2 = int(X_train.shape[0]/5)
+    for i in range(4):
+        clf.partial_fit(X_train.toarray()[contador1 : contador2], y_train[contador1 : contador2], classes = [1,2,3,4,5])
+        contador1+= int(X_train.shape[0]/5)
+        contador2+= int(X_train.shape[0]/5)
 
-    for pliegue in dataset.validation_set:
-        
-        #aqui es donde vamos a realizar el entrenamiento del umbral colocando como target el FPA
-        #el entrenamiento prime    
-
-        i+=1
-
-        # Las lineas comentadas debajo de este renglon, son una propuesta de como usar machine learning para lcasificar, esto se puede usar en la siguiente etapa
-
-        # x = []
-        # xtest = []
-        
-        # for element in pliegue.X_train:
-        #     x.append(str(element))
-
-        # for element in pliegue.X_test:
-        #     xtest.append(str(element))
-
-        # #print(x)
-        # #vamos a utilizar el modelo de clasificacion gaussiana para predecir el fpa, pero para ello, primero debemos de sacar una representacion vectorial binaria
-
-        # #sacando la representacion vectorial----------------------------------------------------------------------------------------------
-
-        # representacionVectorial = vectorizacion.VectorizarFrec(x)
-        # representacionVectorialTest = vectorizacion.VectorizarFrec(xtest)
-
-        print(pliegue.X_train.toarray())
-        print(pliegue.y_train.shape[0])
-        print(type(pliegue.y_train[0]))
-
-        print(pliegue.y_train[0])
-        
-        #-----------------------------------------------------------------------------------------------------------------------------------
-        
-        
-
-        clf.partial_fit(pliegue.X_train.toarray(), pliegue.y_train, classes = [1,2,3,4,5])
-        #clf.fit(pliegue.X_train.toarray(), pliegue.y_train)
-
-        y_predict = clf.predict(pliegue.X_test.toarray())
-
-        print(y_predict)
-        print(pliegue.y_test)
-        print(accuracy_score(pliegue.y_test, y_predict))
-        print(f1_score(pliegue.y_test, y_predict, average = None))
-
-        # #reporte de la clasificacion
-        target_names = ['1','2','3','4','5']
-
-        print(classification_report(pliegue.y_test, y_predict))
-        print (confusion_matrix(pliegue.y_test, y_predict))
-
-        #matriz de confusion
-
-        cm = confusion_matrix(pliegue.y_test, y_predict)
-        print (cm)
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=target_names)
-        disp.plot()
-        plt.show()
+    clf.partial_fit(X_train.toarray()[contador2:], y_train[contador2:], classes = [1,2,3,4,5])
+    
 
     #ahora lo implementamos en el conjunto de prueba
 
-    y_predict = clf.predict(dataset.test_set.X_test.toarray())
+    y_predict = clf.predict(X_test.toarray())
 
     print(y_predict)
-    print(dataset.test_set.y_test)
-    print(accuracy_score(dataset.test_set.y_test, y_predict))
-    print(f1_score(dataset.test_set.y_test, y_predict, average = None))
+    print(y_test)
+    print(accuracy_score(y_test, y_predict))
+    print(f1_score(y_test, y_predict, average = None))
 
     # #reporte de la clasificacion
     target_names = ['1','2','3','4','5']
 
-    print(classification_report(dataset.test_set.y_test, y_predict))
-    print (confusion_matrix(dataset.test_set.y_test, y_predict))
+    print(classification_report(y_test, y_predict))
+    print (confusion_matrix(y_test, y_predict))
 
     #matriz de confusion
 
-    cm = confusion_matrix(dataset.test_set.y_test, y_predict)
+    cm = confusion_matrix(y_test, y_predict)
     print (cm)
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=target_names)
     disp.plot()
