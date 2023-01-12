9ccd526c4390ecdaf09d2dfddf6a64d2763824b5
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2023-01-11 22:20:55 -0600

    version de entrega, segun yo

--- proyecto/main.py
+++ proyecto/main.py
@@ -7,7 +7,6 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from sklearn import svm
 from sklearn.pipeline import FeatureUnion
 import numpy as np
 import json
@@ -106,6 +105,68 @@ def main():
 
     X_train, X_test, y_train, y_test = train_test_split(representacionVectorial, target,test_size=0.2, shuffle=True, random_state=0)
 
+    #dataset = dataSets.crearConjuntosDeValidacion(5, representacionVectorial, target)
+
+    #  for pliegue in dataset.validation_set:
+        
+    #     #aqui es donde vamos a realizar el entrenamiento del umbral colocando como target el FPA
+    #     #el entrenamiento prime    
+
+    #     i+=1
+
+    #     # Las lineas comentadas debajo de este renglon, son una propuesta de como usar machine learning para lcasificar, esto se puede usar en la siguiente etapa
+
+    #     # x = []
+    #     # xtest = []
+        
+    #     # for element in pliegue.X_train:
+    #     #     x.append(str(element))
+
+    #     # for element in pliegue.X_test:
+    #     #     xtest.append(str(element))
+
+    #     # #print(x)
+    #     # #vamos a utilizar el modelo de clasificacion gaussiana para predecir el fpa, pero para ello, primero debemos de sacar una representacion vectorial binaria
+
+    #     # #sacando la representacion vectorial----------------------------------------------------------------------------------------------
+
+    #     # representacionVectorial = vectorizacion.VectorizarFrec(x)
+    #     # representacionVectorialTest = vectorizacion.VectorizarFrec(xtest)
+
+    #     print(pliegue.X_train.toarray())
+    #     print(pliegue.y_train.shape[0])
+    #     print(type(pliegue.y_train[0]))
+
+    #     print(pliegue.y_train[0])
+        
+    #     #-----------------------------------------------------------------------------------------------------------------------------------
+        
+        
+
+    #     clf.partial_fit(pliegue.X_train.toarray(), pliegue.y_train, classes = [1,2,3,4,5])
+    #     #clf.fit(pliegue.X_train.toarray(), pliegue.y_train)
+
+    #     y_predict = clf.predict(pliegue.X_test.toarray())
+
+    #     print(y_predict)
+    #     print(pliegue.y_test)
+    #     print(accuracy_score(pliegue.y_test, y_predict))
+    #     print(f1_score(pliegue.y_test, y_predict, average = None))
+
+    #     # #reporte de la clasificacion
+    #     target_names = ['1','2','3','4','5']
+
+    #     print(classification_report(pliegue.y_test, y_predict))
+    #     print (confusion_matrix(pliegue.y_test, y_predict))
+
+    #     #matriz de confusion
+
+    #     cm = confusion_matrix(pliegue.y_test, y_predict)
+    #     print (cm)
+    #     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=target_names)
+    #     disp.plot()
+    #     plt.show()
+
     #---------------------------------------------------------------------------------------------------------
     # SEL = pd.read_excel("./SEL/SEL.xlsx")
 
@@ -136,8 +197,8 @@ def main():
     # plt.show()
 
     i = 0
-    #clf = LogisticRegression(C=50, solver='lbfgs')
-    clf = svm.SVC(kernel='rbf', gamma=0.5, C=0.1).fit(X_train, y_train)
+    clf = LogisticRegression(C=50, solver='lbfgs')
+    #clf = svm.SVC(kernel='rbf', gamma=0.5, C=0.1).fit(X_train, y_train)
 
     # clf.fit(X_train.toarray(), y_train)
     contador1 = 0
@@ -162,8 +223,8 @@ def main():
     print(f1_score(y_test, y_predict, average = None))
 
     # #reporte de la clasificacion
-    #target_names = ['1','2','3','4','5']
-    target_names = ['Hotel', 'Restaurant', 'Attractive']
+    target_names = ['1','2','3','4','5']
+    #target_names = ['Hotel', 'Restaurant', 'Attractive']
 
     print(classification_report(y_test, y_predict))
     print (confusion_matrix(y_test, y_predict))
